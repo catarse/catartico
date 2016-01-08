9026ed3c5d5aa775f0721cbe00ee535f1e0e30b9
Antônio Roberto <forevertonny@gmail.com>
2016-01-08 12:01:15 -0200

    added boltun to deps

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Catartico.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :boltun]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,6 @@ defmodule Catartico.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:boltun, "~> 0.0.4"}]
   end
 end
