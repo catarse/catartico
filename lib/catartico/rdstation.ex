bfece6742dd49b8e4a32c92c2192ee9a44371d8e
Antônio Roberto <forevertonny@gmail.com>
2016-01-08 18:36:24 -0200

    basic rdstation conversions flow

--- lib/catartico/rdstation.ex
+++ lib/catartico/rdstation.ex
@@ -1,7 +1,9 @@
 defmodule Catartico.Rdstation do
   require Logger
+  require Poison
+  require HTTPoison
 
-  @base_url "https://www.rdstation.com.br/api/1.2"
+  @base_url "https://www.rdstation.com.br/api/1.3"
   @conversions_endpoint "#{@base_url}/conversions"
 
   @moduledoc """
@@ -12,6 +14,26 @@ defmodule Catartico.Rdstation do
   Handles with transformation of payload and sent to rdstation conversions api
   """
   def process(_, payload) do
-    Logger.info "Received listen payload #{payload}"
+    Logger.info "[RD] Received payload #{payload}"
+    payload |> _transform |> _sent
+  end
+
+  defp _transform payload do
+    Logger.info "[RD] Transforming payload -> #payload}"
+    d = Poison.decode!(payload)
+    %{
+      identificador: d["event_name"],
+      email: d["email"],
+      token_rdstation: System.get_env("RD_TOKEN")
+    } |> Poison.encode!
+  end
+
+  defp _sent encoded do
+    Logger.info "[RD] sending enconded event json to rdstation -> #{encoded}"
+    HTTPoison.post(
+      @conversions_endpoint,
+      encoded,
+      [{"Content-Type", "application/json"}]
+    )
   end
 end
