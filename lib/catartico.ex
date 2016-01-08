99c8c2206fb196d2d45e74d8868b9d362a23b3ac
Antônio Roberto <forevertonny@gmail.com>
2016-01-08 14:46:39 -0200

    added boltun listen to conversions

--- lib/catartico.ex
+++ lib/catartico.ex
@@ -1,2 +1,14 @@
 defmodule Catartico do
+  use Boltun, otp_app: :catartico
+  require Logger
+
+  @moduledoc """
+  Listen database and distribute messages to another processes.
+  """
+
+  @conversions_channel "catartico_conversions"
+
+  listen do
+    channel @conversions_channel, Catartico.Conversions, :process, []
+  end
 end
